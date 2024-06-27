ce8d7304db811cc0fcf077644ca7345d5548c853
igorlabzun <155388424+igorlabzun@users.noreply.github.com>
2024-06-27 21:17:02 +0300

    Added class GameControllerService and folder webapp changes

--- src/main/java/org/example/GameControllerService.java
+++ src/main/java/org/example/GameControllerService.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
+@WebServlet("/pony")
 public class GameControllerService extends HttpServlet {
 
     @Override
